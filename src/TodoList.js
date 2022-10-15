b4bb43fe98d9df2317c6caaec99bbfaaa33b1a1c
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-10-15 23:57:35 +0100

    h1

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = (todos: [])
+    }
     render() {
         return (
             <div>
-
+                <h1>Todo List!</h1>
             </div>
         );
     }
