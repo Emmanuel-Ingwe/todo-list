efe8fe959e2442636dd979117ac23ac1c77b8106
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-10-17 04:30:34 +0100

    Edit

--- src/TodoList.js
+++ src/TodoList.js
@@ -10,6 +10,7 @@ class TodoList extends Component {
         };
         this.create = this.create.bind(this);
         this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
     }
     create(newTodo) {
         this.setState({
@@ -21,9 +22,18 @@ class TodoList extends Component {
             todos: this.state.todos.filter(t => t.id !== id)
         });
     }
+    update(id, updatedTask) {
+        const updatedTodos = this.state.todos.map(todo => {
+            if (todo.id === id) {
+                return { ...todo, task: updatedTask };
+            }
+            return todo;
+        });
+        this.setState({ todos: updatedTodos });
+    }
     render() {
         const todos = this.state.todos.map(todo => {
-            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} />;
+            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} updateTodo={this.update} />;
         });
         return (
             <div>
