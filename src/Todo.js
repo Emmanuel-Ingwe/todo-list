3b7f674d25c3ea2930869e792388f7cb25ef2ea3
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-10-17 03:37:20 +0100

    Key

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+    handleRemove() {
+        this.props.removeTodo();
+    }
     render() {
         return (
             <div>
                 <button>Edit</button>
-                <button>X</button>
+                <button onClick={this.handleRemove}>X</button>
                 <li>{this.props.task}</li>
             </div>
         );
--- src/TodoList.js
+++ src/TodoList.js
@@ -6,18 +6,24 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            todos: [{ task: "Walk The Fish" }, { task: "Groom Chickens" }]
+            todos: []
         };
         this.create = this.create.bind(this);
+        this.remove = this.remove.bind(this);
     }
     create(newTodo) {
         this.setState({
             todos: [...this.state.todos, newTodo]
         });
     }
+    remove(id) {
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        });
+    }
     render() {
         const todos = this.state.todos.map(todo => {
-            return <Todo task={todo.task} />;
+            return <Todo key={todo.id} task={todo.task} removeTodo={this.remove} />;
         });
         return (
             <div>
