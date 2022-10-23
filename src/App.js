722121da5838fd6461581a9c76888fe39af2369a
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-10-23 14:42:57 +0100

    Added div

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TodoList />
+      <div>
+        <div className="App">
+          <TodoList />
+        </div>
       </div>
     );
     // return (
